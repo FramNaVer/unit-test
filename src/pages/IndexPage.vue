8db2b9e1970a1fa5a4f0c36b7b4ef06b451ae915
FramNaVer <framnaver@gmail.com>
2023-10-04 12:44:39 +0700

    First-unit-test-jest

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage/>
+  <FuctionPage/>
+
+</template>
+
+
+<script>
+import InputPage from '../pages/InputPage.vue';
+import FuctionPage from '../pages/FunctionPage.vue';
+
+export default {
+    name: 'HtmlComponent',
+    compenent: {
+        InputPage,
+        FuctionPage
+    },
+    data() {
+        return {
+            title: 'Tanadon Inmano 340',
+        };
+    },
+    components: { InputPage,FuctionPage }
+}
+</script>
+
+<style scoped></style>
